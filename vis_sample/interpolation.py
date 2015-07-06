b88a1df1a29e761f267762ae2d46023c9aa0fef8
Ryan Loomis <rloomis@cfa.harvard.edu>
2015-07-06 11:25:30 -0400

    rewrote using vectorization, commented.  several order of magnitude speed increase

--- vis_sample/interpolation.py
+++ vis_sample/interpolation.py
@@ -1,198 +1,102 @@
+# Here we do the interpolation of the fft'd image, and sample at each visibility point.
+# Instead of analytically calculating the gcf for points around each (u,v) sample point
+# we estimate the gcf by precalculating the gcf for a dense grid (1000x image res) and
+# then use these gcf values (for a massive speed increase)
+
 import numpy as np
-from numpy.fft import fftfreq, rfftfreq, fftshift, fft2
 from gridding import *
 from classes import *
-from transforms import *
-import time
-from scipy import weave
-from scipy.weave import converters
+from numpy.lib.stride_tricks import as_strided
 
+
+
+# Calculate the gcf values for 7 fft image points around the dense grid points
+# This is calculated as a 1d problem, calculating u and v weights separately
+# to be multiplied in the future as an outer product.
+#
+# dense_grid_gcf is calculated only once, and then gcf values are retrieved from it
 
 def calc_dense_grid_gcf():
     dense_grid = np.linspace(-0.5, 0.5, 1000)
-    dense_grid_gcf = np.zeros((1000,6))
+    dense_grid_gcf = np.zeros((1000,7))
     for i, grid_pnt in enumerate(dense_grid):
-        eta = (np.arange(-3,3) + grid_pnt)/3.0
-        dense_grid_gcf[i] = gcffun(eta, 1.0)
+        eta = (np.arange(-3,4) + grid_pnt)/3.5
+        dense_grid_gcf[i] = gcffun(eta)
     return dense_grid_gcf
 
-def interpolate_uv_single_point_dense_grid(u, v, l, vis, dense_grid_gcf):
-    # Note that vis.uu goes from positive to negative (East-West)
-    # and vis.vv goes from negative to positive (North-South)
-
-    #t00 = time.time()
-    #t0 = time.time()
-    # 1. Find the nearest gridpoint in the FFT'd image.
-    uvcnt = vis.uu.shape[0]
-    # determine the uu and vv distance for grid points
-    du = vis.uu[1] - vis.uu[0]
-    dv = vis.vv[1] - vis.vv[0]
-
-    iu0 = uvcnt/2+int(u/du+0.5)
-    iv0 = uvcnt/2+int(v/dv+0.5)
-
-    # now find the relative distance to this nearest grid point (not absolute)
-    u0 = u - vis.uu[iu0]
-    v0 = v - vis.vv[iv0]
-
-    # 2. Calculate the appropriate u and v grid points
-    # find the nearest dense grid point
-    iu0_grid = 500+int(1000*u0/du)
-    iv0_grid = 500+int(1000*v0/dv)
-
-    VV = vis.VV[(iv0-3):(iv0+3), (iu0-3):(iu0+3), l] # Array is packed like the image
-
-    #t1 = time.time()
-    #print "total search time = " + str(t1-t0)
-
-    # 3. Calculate the weights corresponding to these 6 nearest pixels (gcffun)
-    #t0 = time.time()
-    uw = dense_grid_gcf[iu0_grid,:]
-    vw = dense_grid_gcf[iv0_grid,:]
-    #t1 = time.time()
-    #print "gcf calc time = " + str(t1-t0)
-
-    all_weights = np.outer(uw, vw)
-
-    # 4. Normalization such that it has an area of 1. Divide by w later.
-    w = (uw[0]+uw[1]+uw[2]+uw[3]+uw[4]+uw[5])*(vw[0]+vw[1]+vw[2]+vw[3]+vw[4]+vw[5])
-
-    # 5. Loop over all 36 grid indices and sum to find the interpolation.
-    cum = np.sum(all_weights*VV)/w
-
-    #t11 = time.time()
-    #print "total single point time = " + str(t11-t00)
-
-    return cum, np.array([(iu0-3), (iu0+3), (iv0-3), (iv0+3)]), all_weights, w
 
 
 
-# called ModGrid in gridding.c (KR code) and in Model.for (MIRIAD)
-# Uses spheroidal wave functions to interpolate a model to a (u,v) coordinate.
-# u,v are in [klam]
-def interpolate_uv_single_point(u, v, l, vis):
-    # Note that vis.uu goes from positive to negative (East-West)
-    # and vis.vv goes from negative to positive (North-South)
+# Caching the gcf values and data indices for a given dataset of (u,v) visibilities results
+# in a ~20% speed increase.  This is implemented through the GCF_holder class.
+#
+# Here we calculated all the gcf values for the visibilities and then pass the values
+# and indices to a GCF_holder
 
-    #t00 = time.time()
-    #t0 = time.time()
-    # 1. Find the nearest gridpoint in the FFT'd image.
-    iu0 = np.argmin(abs(u - vis.uu))
-    iv0 = np.argmin(abs(v - vis.vv))
-    #t1 = time.time()
-    #print "nearest neighbor search time = " + str(t1-t0)
-
-    # now find the relative distance to this nearest grid point (not absolute)
-    u0 = u - vis.uu[iu0]
-    v0 = v - vis.vv[iv0]
-
-    # determine the uu and vv distance for 3 grid points (could be later taken out)
-    du = abs(vis.uu[3] - vis.uu[0])
-    dv = abs(vis.vv[3] - vis.vv[0])
-
-    # 2. Calculate the appropriate u and v indexes for the 6 nearest pixels
-    # (3 on either side)
-    # Are u0 and v0 to the left or the right of the index?
-    # we want to index three to the left, three to the right
-
-    if iu0 < 2 or iv0 < 2 or iu0 > (vis.uu.shape[0]-2) or iv0 > (vis.vv.shape[0]-2):
-        return 0, np.array([0,6,0,6]), np.zeros(36), 1
-    if u0 >= 0.0:
-        # To the right of the index
-        uind = np.arange(iu0-2, iu0+4)
-    else:
-        # To the left of the index
-        uind = np.arange(iu0-3, iu0+3)
-
-    if v0 >= 0.0:
-        # To the right of the index
-        vind = np.arange(iv0-2, iv0+4)
-    else:
-        # To the left of the index
-        vind = np.arange(iv0-3, iv0+3)
-
-    etau = (vis.uu[uind] - u)/(du)
-    etav = (vis.vv[vind] - v)/(dv)
-
-    VV = vis.VV[vind[0]:(vind[5]+1), uind[0]:(uind[5]+1)] # Array is packed like the image
-
-    # 3. Calculate the weights corresponding to these 6 nearest pixels (gcffun)
-    #t0 = time.time()
-    uw = gcffun(etau, 1.0)
-    vw = gcffun(etav, 1.0)
-    all_weights = np.outer(vw, uw)
-    #t1 = time.time()
-    #print "gcf calc time = " + str(t1-t0)    
-
-    # 4. Normalization such that it has an area of 1. Divide by w later.
-    w = np.sum(all_weights)
-
-    # 5. Loop over all 36 grid indices and sum to find the interpolation.
-    cum = 0.+0.j
-    for i in range(6):
-        for j in range(6):
-            cum += uw[i] * vw[j] * VV[j,i,l] # Array is packed like the image
-
-    cum = cum/w
-    
-    #time11 = time.time()
-    #print "total single point time = " + str(t11-t00)
-
-    return cum, np.array([uind[0], (uind[5]+1), vind[0], (vind[5]+1)]), all_weights, w
-
-
-# interpolate a model for many u,v points and store the weights for later
-def interpolate_uv_initial(uu, vv, vis):
-    uvcnt = vv.shape[0]
-    interp_vis = np.zeros((uvcnt), dtype='complex')
-    index_arr = np.zeros((uvcnt, 4))
-    gcf_arr = np.zeros((uvcnt, 6, 6))
-    w_arr = np.zeros((uvcnt))
-
+def create_gcf_holder(uu, vv, vis):
+    nvis = vv.shape[0]
+    viscnt = vis.uu.shape[0]
     dense_grid_gcf = calc_dense_grid_gcf()
 
-    print "interpolating " +str(uvcnt) + " visibilities"
-    for i in range(uvcnt):
-#        interp_vis[i], index_arr[i], gcf_arr[i], w_arr[i] = interpolate_uv_single_point(uu[i], vv[i], 0, vis)
-         interp_vis[i], index_arr[i], gcf_arr[i], w_arr[i] = interpolate_uv_single_point_dense_grid(uu[i], vv[i], 0, vis, dense_grid_gcf)
+    # 1. Find the nearest pixel points in the fft'd image, and push to the index array
+    du = vis.uu[1] - vis.uu[0]
+    dv = vis.vv[1] - vis.vv[0]
+    iu0 = viscnt/2+(uu/du+0.5).astype(int)
+    iv0 = viscnt/2+(vv/dv+0.5).astype(int)
+    index_arr = np.reshape(np.array((iu0, iv0)),(nvis,2))
 
-    return interp_vis, GCF_holder(index_arr, gcf_arr, w_arr, uu, vv)
-    
-# interpolate a model for many u,v points
+    # 2. Find the relative distance to this point (should be -0.5 du/v < val < 0.5 du/v) 
+    u0 = uu - vis.uu[iu0]
+    v0 = vv - vis.vv[iv0]
+
+    # 3. Now we take the relative distance and find the nearest dense grid point
+    iu0_grid = 500+(1000*u0/du).astype(int)
+    iv0_grid = 500+(1000*v0/dv).astype(int)
+
+    # 4. Pull the gcf vals for the nearest 7 pixels around this dense grid point
+    uw = dense_grid_gcf[iu0_grid,:]
+    vw = dense_grid_gcf[iv0_grid,:]
+
+    # 5. Calculate the weights (outer product) and an array of the total weights 
+    all_weights = np.einsum('...a,...b->...ab', vw, uw)
+    w_arr = np.einsum('...ab->...', all_weights)
+
+    return GCF_holder(index_arr, all_weights, w_arr, uu, vv)
+
+
+
+
+# The interpolation call will first calculate the gcf holder (if not provided), and then use
+# those values to calculate the interpolated visibilities. 
+#
+# If the return_gcf flag is set, it returns the gcf holder (possibly useful for future interpolations 
+# where the gcf calculation represents a significant fraction of the computation time).
+
 def interpolate_uv(uu, vv, vis, gcf_holder='', return_gcf=False):
-    t0 = time.time()
+    # create gcf_holder if one isn't provided
     if (gcf_holder==""):
-        uvcnt = vv.shape[0]
-        interp_vis = np.zeros((uvcnt, vis.lams.shape[0]), dtype='complex')
-        interp_vis[:,0], gcf_holder = interpolate_uv_initial(uu, vv, vis)
-        for l in range(1, vis.lams.shape[0]):
-            for i in range(uvcnt):
-                uind0 = gcf_holder.index_arr[i, 0]
-                uind5 = gcf_holder.index_arr[i, 1]
-                vind0 = gcf_holder.index_arr[i, 2]
-                vind5 = gcf_holder.index_arr[i, 3]
-                VV = vis.VV[vind0:vind5, uind0:uind5, l]
-                interp_vis[i,l] = np.sum(gcf_holder.gcf_arr[i]*VV)/gcf_holder.w_arr[i]
-        if (return_gcf==True):
-            t1 = time.time()
-            print "total interpolation time = " + str(t1-t0)
-            return interp_vis, gcf_holder
-        else:
-            t1 = time.time()
-            print "total interpolation time = " + str(t1-t0)
-            return interp_vis
-        
-    else:
-        uvcnt = vv.shape[0]
-        interp_vis = np.zeros((uvcnt, vis.lams.shape[0]), dtype='complex')
-        for l in range(0, vis.lams.shape[0]):
-            for i in range(uvcnt):
-                uind0 = gcf_holder.index_arr[i, 0]
-                uind5 = gcf_holder.index_arr[i, 1]
-                vind0 = gcf_holder.index_arr[i, 2]
-                vind5 = gcf_holder.index_arr[i, 3]
-                VV = vis.VV[vind0:vind5, uind0:uind5, l]
-                interp_vis[i,l] = np.sum(gcf_holder.gcf_arr[i]*VV)/gcf_holder.w_arr[i]
+        t0 = time.time()
+        gcf_holder = create_gcf_holder(uu, vv, vis)
         t1 = time.time()
-        print "total interpolation time = " + str(t1-t0)
+        print "gcf creation time = " + str(t1-t0)
+
+    # create the new interpolated visibility holder
+    nvis = vv.shape[0]
+    interp_vis = np.zeros((nvis, vis.lams.shape[0]), dtype='complex')
+    
+    # read out the pixel indices for windowing
+    u0 = gcf_holder.index_arr[:, 0].astype(int)-4
+    v0 = gcf_holder.index_arr[:, 1].astype(int)-4
+
+    # iterate through the channels and multiply weights by the windowed pixels
+    # note that we could theoretically vectorize by channel as well, but this
+    # produces a memory error - maybe the array is too large?
+    for l in range(vis.lams.shape[0]):
+        VV_chan = vis.VV[:,:,l]
+        VV = as_strided(VV_chan, shape=(VV_chan.shape[0]-5, VV_chan.shape[1]-5, 7, 7), strides=VV_chan.strides * 2)
+        interp_vis[:,l] = np.einsum("...ab->...", gcf_holder.gcf_arr*VV[v0,u0])/gcf_holder.w_arr
+
+    if (return_gcf==True):
+        return interp_vis, gcf_holder
+    else:
         return interp_vis
