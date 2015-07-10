1f7266ee1d74399c724a4bb29eab6da5313b474c
Ryan Loomis <rloomis@cfa.harvard.edu>
2015-07-10 15:15:43 -0400

    added padding to prevent edge effects and fixed the way uu/vv distances were calculated

--- vis_sample/transforms.py
+++ vis_sample/transforms.py
@@ -1,56 +1,71 @@
+# Here we define all of the necessary functions to FFT the model image and phase shift visibilities
 import numpy as np
 from constants import *
 from numpy.fft import fftfreq, rfftfreq, fftshift, fft2
 from gridding import *
 from classes import *
 
-# Given a new model centroid in the image plane (in arcseconds), shift the
-# visibilities by corresponding amount
+
+# Transform the SkyImage using FFT and return a Model_Visibility
+def transform(img):
+     # we pad to prevent edge effects during interpolation
+    full_fft = np.zeros(((img.data.shape[0]+4),(img.data.shape[1]+4), img.data.shape[2]), dtype='complex')
+
+    # convert ra and dec in [arcsec] to radians, and then take the sin to
+    # convert to ll, mm
+
+    ll = np.sin(img.ra*arcsec)
+    mm = np.sin(img.dec*arcsec)
+
+    # number of elements in each array
+    nl = ll.shape[0]
+    nm = mm.shape[0]
+
+    # find the spacing between the elements, as long as we're not 1D
+    if nl > 1:
+        dl = abs(ll[1] - ll[0]) # [radians]
+    else:
+        dl = 1.0
+
+    if nm > 1:
+        dm = abs(mm[1] - mm[0]) # [radians]
+    else:
+        dm = 1.0
+
+    # determine uv plane coordinates in lambda
+    uu = fftshift(fftfreq(nl, dl))
+    vv = fftshift(fftfreq(nm, dm))
+
+    for chan in range(img.freqs.shape[0]):
+        data = img.data[::-1, :, chan]
+
+        # properly pack the data for input using fftshift to move the component at
+        # RA=0,DEC=0 to the first array element: data[0,0]
+        chan_fft = fftshift(fft2(fftshift(data)))
+        full_fft[2:-2,2:-2,chan] = chan_fft
+
+        # we pad to prevent edge effects during interpolation
+        uu_pad = np.lib.pad(uu, (2,2), 'constant')
+        vv_pad = np.lib.pad(vv, (2,2), 'constant')
+
+    return ModelVisibility(full_fft, uu_pad, vv_pad, img.freqs)
+
+
+
+
+
+
+# This function lets us shift visibilities given a new centroid (in arcsec)
 def phase_shift(vis, mu_RA, mu_DEC):
     mu = np.array([mu_RA, mu_DEC]) * arcsec # [radians]
     nu = vis.uu.shape[0]
     nv = vis.vv.shape[0]
     
     # Go through each visibility and apply the phase shift
-    for l in range(vis.lams.shape[0]):
+    for l in range(vis.freqs.shape[0]):
         for i in range(nu):
             for j in range(nv):
-                # Convert from [klam] to [lam]
-                R = np.array([vis.uu[i], vis.vv[j]]) * 1e3 #[lam]
+                R = np.array([vis.uu[i], vis.vv[j]])
                 # Not actually in polar phase form
                 shift = np.exp(-2*pi*1.0j * np.sum(R*mu))
                 vis.VV[j,i,l] = vis.VV[j,i,l] * shift
-
-
-
-# Transform the SkyImage using FFT
-def transform(img):
-    full_out = np.zeros(img.data.shape, dtype='complex')
-
-    # convert ra and dec in [arcsec] to radians, and then take the sin to
-    # convert to ll, mm
-    ll = np.sin(img.ra * arcsec)
-    mm = np.sin(img.dec * arcsec)
-
-    # number of elements in each array
-    nl = ll.shape[0]
-    nm = mm.shape[0]
-
-    # find the spacing between the elements
-    dl = abs(ll[1] - ll[0]) # [radians]
-    dm = abs(mm[1] - mm[0]) # [radians]
-
-    # determine uv plane coordinates in klam
-    uu = fftshift(fftfreq(nl, dl)) * 1e-3 # [klam]
-    vv = fftshift(fftfreq(nm, dm)) * 1e-3 # [klam]
-
-    for index in range(img.lams.shape[0]):
-        data = img.data[::-1, :, index]
-
-        # properly pack the data for input using fftshift to move the component at
-        # RA=0,DEC=0 to the first array element: data[0,0]
-
-        chan_out = fftshift(fft2(fftshift(data)))
-        full_out[:,:,index] = chan_out
-
-    return Model_Visibility(full_out, uu, vv, img.lams)
