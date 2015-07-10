0c6606e1384475b67b173e833294618d9d440f80
Ryan Loomis <rloomis@cfa.harvard.edu>
2015-07-10 15:12:41 -0400

    renamed lams to freqs, documented better

--- vis_sample/classes.py
+++ vis_sample/classes.py
@@ -8,35 +8,48 @@ from gridding import *
 # corner *and* flip the array along the RA axis: `fliplr(data)`
 
 class SkyImage:
-    def __init__(self, data, ra, dec, lams):
+    def __init__(self, data, ra, dec, freqs):
         if len(data.shape) < 3:
             self.data = np.reshape(data, (data.shape[0], data.shape[1], 1))
         else:
             self.data = data
         self.ra = ra
         self.dec = dec
-        self.lams = np.array(lams)
+        self.freqs = np.array(freqs)
 
-# data visibility is stored as VV[index, l]
+
+
+# data visibility is stored as VV[index, f]
 # ie there is no regular grid, just a list
+# therefore uu.shape == vv.shape
+
 class Visibility:
-    def __init__(self, VV, uu, vv, wgts, lams):
+    def __init__(self, VV, uu, vv, wgts, freqs):
         self.VV = VV
         self.uu = uu
         self.vv = vv
         self.wgts = wgts
-        self.lams = np.array(lams)
+        self.freqs = np.array(freqs)
 
-# model visibilities aren't weighted
-# they also are stored VV[j, i, l]
-class Model_Visibility:
-    def __init__(self, VV, uu, vv, lams):
+
+
+# model visibilities aren't weighted and are on a regular grid
+# they also are stored VV[v, u, f]
+# uu and vv can be different sizes
+
+class ModelVisibility:
+    def __init__(self, VV, uu, vv, freqs):
         self.VV = VV
         self.uu = uu
         self.vv = vv
-        self.lams = np.array(lams)
+        self.freqs = np.array(freqs)
 
-class GCF_holder:
+
+# The GcfHolder allows us to cache the several values for future calls
+# it holds the pixel indexes for the dataset, the gcf values, and the total weights
+# additionally we hold the uu, vv values, so that we can avoid importing the data multiple times
+
+class GcfHolder:
     def __init__(self, index_arr, gcf_arr, w_arr, uu, vv):
         self.index_arr = index_arr
         self.gcf_arr = gcf_arr
